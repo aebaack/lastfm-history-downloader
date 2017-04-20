523040eedda4775b64d6df33626f9104501ed0f2
aebaack <aebaack@gmail.com>
2017-04-20 16:26:32 -0600

    Make additional API calls if the limit is reached

--- downloader/lastfm_downloader.py
+++ downloader/lastfm_downloader.py
@@ -1,5 +1,5 @@
 import requests
-import json
+#import json
 
 class LastDownloader():
 	"""A listening history downloader for Last.fm"""
@@ -11,26 +11,31 @@ class LastDownloader():
 		self.base_url = 'http://ws.audioscrobbler.com/2.0/?'
 		
 	def get_all_artists(self):
-		"""Get all artists artists for account"""
-		
-		# Format API call
+		"""Get all artists artists for account"""		
+		# Format API call and send request
 		request_params = {
 			'limit': '1000',
 			'method': 'library.getartists',
 			'user': self.username,
 			}
-		request_url = self.format_api_request(request_params)
+		response = self.send_api_request(request_params)
 		
-		# Send request
-		r = requests.get(request_url)
-		if r.status_code != 200:
-			print("Error with request")
-		response = r.json()
+		artists_list = response['artists']['artist']
+		
+		# A single request is limited to the first 1000 artists,
+		# so additional calls may be needed to populate entire list
+		total_info = response['artists']['@attr']
+		while int(total_info['page']) < int(total_info['totalPages']):
+			request_params['page'] = str(int(total_info['page'])+1)
+			response = self.send_api_request(request_params)
+			artists_list += response['artists']['artist']
+			total_info = response['artists']['@attr']
+		
+		return artists_list
 		
 		# Display response
-		print(json.dumps(response, indent=4, sort_keys=True))
-		artists_list = response['artists']['artist']
-		print(len(artists_list))
+		#print(json.dumps(response, indent=4, sort_keys=True))
+		#print(len(artists_list))
 		
 	def format_api_request(self, params_dict):
 		"""Format API request"""
@@ -39,6 +44,14 @@ class LastDownloader():
 		for param, value in params_dict.items():
 			base_url += '&' + param + '=' + value
 		return base_url
+		
+	def send_api_request(self, params_dict):
+		"""Send API request with given parameters"""
+		request_url = self.format_api_request(params_dict)
+		r = requests.get(request_url)
+		if r.status_code != 200:
+			print("Error with request")
+		return r.json()
 		
 download_user = LastDownloader('***REMOVED***')
 download_user.get_all_artists()
