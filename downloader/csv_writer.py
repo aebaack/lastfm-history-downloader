c22e9eb843d8a35aedf7a66202d82a271caa640d
aebaack <aebaack@gmail.com>
2017-04-22 11:36:55 -0600

    Create csv writer object

--- downloader/csv_writer.py
+++ downloader/csv_writer.py
@@ -1,1 +1,22 @@
+import csv
+import os.path
 
+class CSVWriter():
+	"""Write tracks to a CSV file"""
+	
+	def __init__(self, file_path='./scrobbles.csv'):
+		"""Initialize file name where data is saved"""
+		self.file_path = file_path
+	
+	def return_csv_writer(self):
+		"""Return csv writer object"""
+		access_mode = 'wb'
+		if os.path.isfile(self.file_path):
+			access_mode = 'ab'
+			
+		with open(self.file_path, access_mode) as csv_file:
+			return csv.writer(csv_file)
+	
+	#def write_track_to_csv(self, track):
+		
+writer = CSVWriter()
