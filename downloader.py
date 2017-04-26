6e7151baa660c411b3bb1253355aaf1c83d520bc
aebaack <aebaack@gmail.com>
2017-04-26 11:38:29 -0600

    Comment code

--- downloader.py
+++ downloader.py
@@ -32,96 +32,101 @@ def find_artist_songs(api_wrapper, artist):
 	name = artist['name']
 	display_current_artist(name)
 	print(' - Determining scrobbled tracks')
-	
+
 	# Determine scrobbling information for given artist
 	artist_scrobbles = api_wrapper.get_all_artist_scrobbles(name)
 	form_scrob = api_wrapper.format_artist_tracks(artist_scrobbles)
-	
+
 	# Display number of tracks to search for
 	print(' - Searching for total scrobble count for ' + 
 			str(len(form_scrob)) + ' tracks')
-	
+
 	# Traverse list of formatted scrobbles and total track play count
 	completed_tracks = []
 	for track in form_scrob:
 		# Get total scrobbles for track
 		total_scrob = api_wrapper.get_total_track_scrobbles(track)
-		
+
 		# Add additional plays until list of recent scrobbles is as
 		# long as the list of total scrobbles
 		# This ensures that imported scrobbles are not ignored
 		track.add_imported_plays(total_scrob - len(track.plays))
-		
+
 		completed_tracks.append(track)
-	
+
 	# Search through top 100 for any missed tracks
 	missed = search_artist_top_100(api_wrapper, completed_tracks, name)
 	completed_tracks += missed
-	
+
 	return completed_tracks
-	
+
 def search_artist_top_100(api_wrapper, comp_tracks, artist):
 	"""Search top 100 tracks by artist to find additional scrobbles"""
 	# Determine total artist scrobbles to compare against logged amount
-	total_art_scrob = api_wrapper.get_total_artist_scrobbles(artist)
-	
+	total_artist_scrob = api_wrapper.get_total_artist_scrobbles(artist)
+
 	# Determine total completed scrobbles for the artist
 	comp_count = 0
 	for track in comp_tracks:
 		comp_count += len(track.plays)
-	
-	# If logged amount is less than total, search through top tracks to
-	# find additional missed scrobbles
-	new_tracks = []
-	if comp_count < total_art_scrob:
+
+	# If completed amount is less than total, search through top tracks
+	# to find additional missed scrobbles
+	missed_tracks = []
+	if comp_count < total_artist_scrob:
 		print(' - Searching through top 100 songs to find any missed' +
 				' tracks')
 		top_tracks = api_wrapper.get_all_songs_by_artist(artist)
-		
+
 		counter = 0
 		for track in top_tracks:
+			# Determine if track has already been completed
 			track_already_completed = False
-			
+
 			for comp_track in comp_tracks:
 				if track['name'] == comp_track.song:
 					track_already_completed = True
-					
+
+			# If the track has not been completed, create a new track
+			# and add the necessary amount of plays
 			if not track_already_completed:
 				new_track = Track({'#text': artist}, 'test', track['name'], '')
 				total = api_wrapper.get_total_track_scrobbles(new_track)
 				print(total)
-				
+
 				if total > 0:
 					new_track.add_imported_plays(total)
 					counter += total
-						
-					new_tracks.append(new_track)
-					
+
+					missed_tracks.append(new_track)
+
+		# Display amount of missing tracks that were found
 		print(' - Found ' + str(counter) + ' plays out of the ' +
-				'missing ' + str(total_art_scrob - comp_count))
-	return new_tracks
-			
+				'missing ' + str(total_artist_scrob - comp_count))
+
+	# Return any new tracks or an empty list
+	return missed_tracks
 
 def main():
 	"""Download listening history for input user"""
 	display_introduction()
-	
+
 	# Create new instance of APIWrapper
 	user = determine_credentials()
 	scrobbles = APIWrapper(user['api_key'], user['username'])
-	
+
 	# Determine list of all scrobbled artists for the user
 	print('\n - Searching for all artists for ' + user['username'])
 	all_artists = scrobbles.get_all_artists()
 	print(' - Found ' + str(len(all_artists)) + ' artists')
-	
+
 	# Create new CSVWriter
 	if user['save_location']:
 		writer = CSVWriter(user['save_location'])
 	else:
 		writer = CSVWriter('./' + user['username'] + '.csv')
 	writer.override_previous_csv()
-	
+
 	# Determine total listening history for each track
 	all_tracks = []
 	for artist in all_artists:
